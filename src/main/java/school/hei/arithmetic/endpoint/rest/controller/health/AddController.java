6ac3a7101f1e7ce393e7c7d40b95f01f109b0241
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-24 09:20:33 +0300

    feat: addition controller

---
+++ src/main/java/school/hei/arithmetic/endpoint/rest/controller/health/AddController.java
@@ -0,0 +1,13 @@
+package school.hei.arithmetic.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddController {
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+}
